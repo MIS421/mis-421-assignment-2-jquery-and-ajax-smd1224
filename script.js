22df50d85c2ff799660634e0f095a591d20c8c7e
Sean Daly <smdaly@crimson.ua.edu>
2021-02-10 14:42:50 -0600

    1st commit

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function apiSearch() {
   };
 
   $.ajax({
-      url: 'my-api-url' + $.param(params),
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
       beforeSend: function (xhrObj) {
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "6660198a62a44b559902732f48a946cb");
       },
       type: "GET",
     })
