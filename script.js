d274b98e8c37932dc03d0514c2aa875d56bf206e
Sean Daly <smdaly@crimson.ua.edu>
2021-02-11 15:48:07 -0600

    final commit

--- script.js
+++ script.js
@@ -25,12 +25,19 @@ function apiSearch() {
       }
 
       $('#searchResults').html(results);
+
       $('#searchResults').dialog();
 
     })
     .fail(function () {
       alert("error");
     });
+}
+
+function runSearch(){
+    apiSearch();
+    $('#searchResults').css("visibility", "visible");
+    results = '';
 }
 
 function displayTime() {
@@ -45,6 +52,21 @@ function displayTime() {
     var currentTime = "<h2>" + time + "</h2>"
 
     $('#time').html(currentTime);
+    $('#time').css("visibility", "visible");
     $('#time').dialog();
 
+
 }
+
+var imageUrl = ["https://images.unsplash.com/photo-1532673492-1b3cdb05d51b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+    "https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"];
+
+function imageSwap() {
+    var temp = imageUrl[0];
+    imageUrl[0] = imageUrl[1];
+    imageUrl[1] = temp;
+
+    $("body").css("background-image", "url(" + imageUrl[1] + ")");
+}
+
+
